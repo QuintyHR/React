9d1733bb113de6f1d9756a6f73e9267ddd9bb5a2
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-12-20 11:12:20 +0100

    first commit

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import "./styles.css"
+
+export function About(props) {
+    return <div className="About">
+        <h2>About {props.name}</h2>
+    </div>
+}
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css"
+import { About } from "./About"
+
+export function App() {
+    return <div className="App">
+        <h1>Hello React!</h1>
+        <About name = "Quinty" />
+    </div>
+}
